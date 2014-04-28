3df640e3b9ad2705b8d0b4d2cf10537a3123d848
guerremdq <guerremdq@gmail.com>
2014-04-28 15:21:20 -0300

    Some lints

--- graphite-check.py
+++ graphite-check.py
@@ -3,46 +3,46 @@ from optparse import OptionParser
 import sys
 import requests
 
-
 options = OptionParser()
-options.add_option("-u", "--url"      , dest="url",help="graphite url", default="http://graphite.example.com")
-options.add_option("-c", "--critical", 	dest="critical",help="critical limit", default="1024" , type ="int")
-options.add_option("-w", "--warning", 	dest="warning",help="warning limit",  default="800" , type = "int")
-options.add_option("-t", "--target", 	dest="target",help="graphite target", default="server.web1.load.load.longterm")
-options.add_option("-T", "--time", 	dest="time",help="time in minutes", default="5", type="int")
-
+options.add_option("-u", "--url", dest="url", help="graphite url",
+                    default="http://graphite.example.com")
+options.add_option("-c", "--critical", dest="critical", help="critical limit",
+                    default="1024", type="int")
+options.add_option("-w", "--warning", dest="warning",
+                    help="warning limit", default="800", type="int")
+options.add_option("-t", "--target", dest="target",
+                    help="graphite target",
+                    default="server.web1.load.load.longterm")
+options.add_option("-T", "--time", dest="time", help="time in minutes",
+                    default="5", type="int")
 (options, args) = options.parse_args()
-
-URL=("%s/render/?target=%s&from=-%dminutes&format=json" % ( options.url , options.target , options.time ))
+URL = ("%s/render/?target=%s&from=-%dminutes&format=json" %
+        (options.url, options.target, options.time))
 try:
-	r  = requests.get(URL).json()
-
+    r = requests.get(URL).json()
 except  requests.exceptions.ConnectionError:
-	print "Critial : Connection Error"
-	sys.exit(1)
-
+    print ("Critial : Connection Error")
+    sys.exit(1)
 i = 0
 total = 0
 
-for x in r :
-	for d in x['datapoints']: 	
-		if d[0] != None:
-			total = total + d[0]
-		i = i + 1 
-avg = total / i 
+for x in r:
+    for d in x['datapoints']:
+        if d[0] is not None:
+            total = total + d[0]
+            i = i + 1
+
+avg = total / i
 
 if avg >= options.critical:
-	print "Critical Avg : %d " % avg
-	sys.exit(1)
+    print (("Critical Avg : %d " % avg))
+    sys.exit(1)
 elif avg >= options.warning:
-	print "Warning Avg : %d " % avg
-	sys.exit(2)
+    print (("Warning Avg : %d " % avg))
+    sys.exit(2)
 elif avg < options.warning:
-	print "OK Avg : %i " % avg
-	sys.exit(0)
+    print (("OK Avg : %i " % avg))
+    sys.exit(0)
 else:
-	print "Unknow"
-	sys.exit(3)
-
-
-
+    print ("Unknow")
+    sys.exit(3)
